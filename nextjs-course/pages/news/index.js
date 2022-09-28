d3d477df42a19d538db62771bc928c0717f5248c
prashanthdsouza <79136584+prashanthdsouza@users.noreply.github.com>
2022-09-28 14:31:50 +0530

    328

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,8 +1,19 @@
 // our-domain.com/news
-
+import Link from "next/link";
+import { Fragment } from "react";
 
 function NewsPage() {
-    return <h1>The News page</h1>
+  return (
+    <Fragment>
+      <h1>The News page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-framewrok">NextJs framework</Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
