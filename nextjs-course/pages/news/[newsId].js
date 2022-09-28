710862f21613a1e3ed40627578cae0e789ed078e
prashanthdsouza <79136584+prashanthdsouza@users.noreply.github.com>
2022-09-28 14:11:08 +0530

    327

--- nextjs-course/pages/news/[newsId].js
+++ nextjs-course/pages/news/[newsId].js
@@ -1,7 +1,12 @@
+import {useRouter} from 'next/router';
+
 // our-domain.com/news/something-important
 
-
 function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
     return <h1>The Detail page</h1>
 }
 
